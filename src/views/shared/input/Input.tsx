2601ab994c64b6a0ad350cd784370cdf3fe66787
arifdroid <arifdroiddeveloper@gmail.com>
2021-08-31 15:31:22 +0800

    styling for input

--- src/views/shared/input/Input.tsx
+++ src/views/shared/input/Input.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import { TextField } from '@material-ui/core';
+import React, { useState } from 'react';
+import { createStyles, InputBase, makeStyles, TextField, Theme } from '@material-ui/core';
 import { Search } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        inputStyle: {
+            color: "black",
+            fontWeight:420            
+        },
+    }),
+);
+
 
 
 export function Input(props) {
+
     const {
         label,
         name,
@@ -18,40 +29,48 @@ export function Input(props) {
         endAdornment,
     } = props;
 
+    const classes = useStyles();
+
+    const [isFocus, setIsFocus] = useState(false)
+
     return (
         <TextField
             id={name}
             name={name}
             type={type}
             label={label}
+            color={'secondary'}
             style={{
-                backgroundColor:'rgba(234,240,240,0.40)',
-                width:400,
-                
+                backgroundColor: 'rgba(234,240,240,0.60)',
+                width: 400,
+                borderRadius: 3,
+                borderColor: 'white'
 
             }}
+
             required={required}
-            //   inputRef={register}
             onChange={(event) => {
                 props.onChange &&
                     props.onChange(event.target.value);
+
             }}
             onBlur={(event) => {
                 props.onBlur && props.onBlur(event);
             }}
-            margin="normal"            
+            margin="normal"
             variant="outlined"
             size="small"
             placeholder={placeholder || undefined}
             autoFocus={autoFocus || undefined}
-            autoComplete={autoComplete || undefined}            
-            
+            autoComplete={autoComplete || undefined}
             InputProps={{
+                className: classes.inputStyle,
                 endAdornment:
-                    <Search />
+                    <Search color={'secondary'} />
             }}
             disabled={disabled}
         />
+
     );
 }
 
